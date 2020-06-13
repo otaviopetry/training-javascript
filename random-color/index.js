4eee42ca2ca92538b8b1b232c14da8a000c25d30
otaviopetry <otaviopetry@gmail.com>
2020-06-12 22:15:41 -0300

    fix: Commenting the code

--- random-color/index.js
+++ random-color/index.js
@@ -1,20 +1,27 @@
 const generator = document.querySelector('#generator');
 const squareContainer = document.querySelector('.squareContainer');
 
+// the generator button listener
 generator.addEventListener( 'click', function () {
     createSquare();
 } );
 
 function createSquare () {
     
+    // create the element and set its class
     const square = document.createElement('div');
-
     square.setAttribute('class', 'square');
     
+    // append it as a child of the container
     squareContainer.appendChild(square);
 
+    // set the event listener for the created element
     square.addEventListener('mouseover', function (evt) {
+
+        // generate the new color rgb value
         var newColor = randomColor();
+
+        // set the new background color
         evt.target.style.backgroundColor = newColor;
     })
 }
@@ -26,4 +33,5 @@ function randomColor () {
     let b = Math.ceil(Math.random() * 255);
 
     return(`rgb(${r}, ${g}, ${b})`);
+    
 }
